6da5eeb8278a2c4677a623c03a1b9863120f63ee
Yunia <yuniawang27@gmail.com>
2026-02-25 17:11:47 +0800

    Add self-introduction feature

---
+++ src/main/kotlin/jiayuwang/SelfIntroduction.kt
@@ -0,0 +1,10 @@
+package hk.edu.hkbu.comp.comp4107.playground.jiayuwang
+
+class SelfIntroduction {
+    fun selfIntroduction() {
+        println("Hello, my name is Jiayu Wang.")
+        println("I am a student at HKBU.")
+        println("I am studying Computer Science.")
+        println("I am interested in machine learning.")
+    }
+}
